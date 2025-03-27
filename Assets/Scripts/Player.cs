e02b9fbce47c9e279c78910ebe9ac6b9b4f5828d
MaximilianoPisani <pisanimaximiliano1@gmail.com>
2025-03-27 10:38:39 -0300

    commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,29 +4,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Velocidad del personaje
+    public float moveSpeed = 5f;
+    public int maxHealth = 5;
+    private int _currentHealth;
+    private bool _canTakeDamage = true;
+    public float damageCooldown = 1.5f;
 
     private Rigidbody2D _rb;
     private Vector2 _movement;
+    private Animator _animator;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _currentHealth = maxHealth;
     }
 
     void Update()
     {
-        
+
         _movement.x = Input.GetAxisRaw("Horizontal");
         _movement.y = Input.GetAxisRaw("Vertical");
 
-        
+
         _movement = _movement.normalized;
+
+
+        _animator.SetFloat("Horizontal", _movement.x);
+        _animator.SetFloat("Vertical", _movement.y);
+
+
+        bool isWalking = _movement.x != 0 || _movement.y != 0;
+        _animator.SetBool("IsWalking", isWalking);
     }
 
     void FixedUpdate()
     {
-        // Aplica movimiento usando Rigidbody2D
+
         _rb.velocity = _movement * moveSpeed;
     }
 }
